2c5a4b610d2a6f363517b807580d4d7c1dc52683
Renae Rogers <renae.a.rogers@gmail.com>
2026-05-28 13:42:01 -0600

    created skeleton code for the scripture class

---
+++ Scripture.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Scripture
+{
+    // This method will be used to temporarily store and display the scripture reference and text
+    // The scripture will be run through this program before being used in the scripture memorization program
+    static void Reference(string[] args)
+    {
+        string reference = "";
+    }
+    static void Passage(string[] args)
+    {
+        string passage = "";
+    }
+}
