e9dca331fea4b974c2e0bcb93c737b9312e8bb12
Sean Gillman <fruit@ThinkingRocks.(none)>
2026-06-02 12:17:13 -0600

    new project ver

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ class Program
     }
     static private int menu(string libname)
     {
-        Console.WriteLine("Choose an option or type 'quit' to exit:\n1) Learn random scripture\n2)Learn chosen scripture (must be in library)\n 3) Save new Scripture");
+        Console.WriteLine("Choose an option or type 'quit' to exit:\n1) Learn random scripture\n2) Learn chosen scripture (must be in library)\n3) Save new Scripture");
         string input = Console.ReadLine();
         int c;
         while (input != "quit" &&
@@ -33,34 +33,33 @@ class Program
         switch (c)
         {
             case 1://Learn random
-            Random num = new Random();
-            int scrindex = num.Next(1,File.ReadAllLines("file.txt").Length);
-            // find new scripture in lib from rand index
-            // open scripture
+                Random num = new Random();
+                int scrindex = num.Next(1,File.ReadAllLines(libname).Length);
+                // find new scripture in lib from rand index
+                // open scripture
+                break;
 
             case 2: //learn chosen
                 Console.WriteLine("Enter a reference in the form John 1:5 or Moroini 7:47-48\nEnsure there are capitals and spaces where needed");
                 string sref = Console.ReadLine();
-                
                 // open scripture 
+                break;
 
             case 3://Save
-            Console.WriteLine("Enter a reference in the form John 1:5 or Moroini 7:47-48\nEnsure there are capitals and spaces where needed");
-            string nref = Console.ReadLine();
-            Console.WriteLine("Enter the complete passage text");
-            string npas = Console.ReadLine();
-            File.AppendAllText(libname, $"{nref}||{npas}" + Environment.NewLine);
+                Console.WriteLine("Enter a reference in the form John 1:5 or Moroini 7:47-48\nEnsure there are capitals and spaces where needed");
+                string nref = Console.ReadLine();
+                Console.WriteLine("Enter the complete passage text");
+                string npas = Console.ReadLine();
+                File.AppendAllText(libname, $"{nref}|{npas}" + Environment.NewLine);
+                break;
         }
         return c;
   
     }
-    static private Scripture getscript(string library,int index)
+    public static Scripture GetScript(string library, int index)
     {
-        string line = File.ReadLines(library).Skip(index-1).First();
-        
-        string[] parts = input.Split('|');
-        string reference = parts[0];
-        string passage = parts[1];
-        Scripture script = new Scripture(reference,passage);
+        string line = File.ReadLines(library).Skip(index - 1).First();
+        var parts = line.Split('|', 2);
+        return new Scripture(parts[0], parts[1]);
     }
 }
