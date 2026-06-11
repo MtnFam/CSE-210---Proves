5191ae72d75f83805444fe76d30bd3794eea0e9f
Sean Gillman <fruit@ThinkingRocks.(none)>
2026-06-11 15:09:57 -0600

    activity class ver1.0

---
+++ Mindful/activity.cs
@@ -0,0 +1,41 @@
+/*
+Name: Sean Gillman
+Date: 6/11/2026
+*/
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Reflection;
+
+class Activity
+{
+    //breathing, reflection, listing
+    private List<string> _intro = ["This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.","This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.","This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area."];
+    private List<string> _name = ["Breathing Activity","Refleciton Activity","Lisitng ACtiviity"];
+    public void run(int actnum)
+    {
+        int time = intro(actnum);
+        switch (actnum)
+        {
+            case 0://breathe
+            Breathe br = new Breathe(time);
+            break;
+            case 1://listing
+            Reflection re = new Reflection(time);
+            break;
+            case 2://listing
+            Listing li = new Listing(time);
+            break;
+        }
+    }
+    private int intro(int actnum)
+    {
+        Console.WriteLine($"Welcome to the {_name[actnum]}.\n\n{_intro[actnum]}\nFor how long, in seconds, would you like for your session?");
+        int time;
+        while (!int.TryParse(Console.ReadLine(), out time))
+        {
+            Console.WriteLine("Please enter a valid whole number:");
+        }
+        return time;
+        }
+}
