eef8285fc5ff0e710dc469aba1c6683c9c585c1e
Sean Gillman <fruit@ThinkingRocks.(none)>
2026-06-15 14:05:03 -0600

    simplified breathe class and other small bug fixes

--- Mindful/breathe.cs
+++ Mindful/breathe.cs
@@ -18,36 +18,31 @@ class Breathe
     {
         int leftover = _time%8;
         int cycle = _time/8;
+        string[] breath = ["Breathe in...", "Breathe out..."];
         for (int i = cycle; i > 0; i--)
         {
-            Console.WriteLine("Breathe in...");
-            Thread.Sleep(1000);
-            Console.WriteLine("1...");
-            Thread.Sleep(1000);
-            Console.WriteLine("2...");
-            Thread.Sleep(1000);
-            Console.WriteLine("3...");
-            Thread.Sleep(1000);
-            Console.WriteLine("Breathe out");
-            Thread.Sleep(1000);
-            Console.WriteLine("1...");
-            Thread.Sleep(1000);
-            Console.WriteLine("2...");
-            Thread.Sleep(1000);
-            Console.WriteLine("3...");
-            Thread.Sleep(1000);
+            foreach (var phase in breath)
+            {
+                Console.WriteLine(phase);
+                Thread.Sleep(1000);
+
+                for (int count = 1; count <= 3; count++)
+                {
+                    Console.WriteLine($"{count}...");
+                    Thread.Sleep(1000);
+                }
+            }
         }
         //adds 4 seconsd to end of looping but will lways be within 3s of given time
-        for (int i = leftover; i > 0; i--)
+        if (leftover> 0)
         {
-            Console.WriteLine("Breathe in...");
-            Thread.Sleep(1000);
-            Console.WriteLine("1...");
-            Thread.Sleep(1000);
-            Console.WriteLine("Breathe out");
-            Thread.Sleep(1000);
-            Console.WriteLine("1...");
-            Thread.Sleep(1000);
+            foreach (var phase in breath)
+            {
+                Console.WriteLine(phase);
+                Thread.Sleep(1000);
+                Console.WriteLine("1...");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
