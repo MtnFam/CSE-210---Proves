e4aa4375e4fc397579ef2b4d88671b912643a597
Liam Barber <barber.liam99@gmail.com>
2026-06-15 21:38:32 -0600

    Essentially complete, still needs testing

--- Mindful/listing.cs
+++ Mindful/listing.cs
@@ -1,33 +1,47 @@
 using System;
+using System.Diagnostics;
 using System.Dynamic;
 using System.Globalization;
 using System.IO.Compression;
 
-class Listing
+class Listing : Activity
 {
     // Variables and Initialization
     private List<string> _prompt = new List<string>() {"Who are the peoaple that you appreciate?", "What are personal strengths of yours?", "Whoa are peopeal that you have halped this week?", "When have you felt the Holy Ghost today?", "Who are your personal heroes?"};
+    private int _promptDisp;
     private List<string> response = new List<string>();
     private string _promptChosen;
     private int _time;
 
 
-    public Listing(int times)
+    public Listing(int times) : base (int )
     {
         _time = times * 1000;
     }
-    public void DisplayPrompt()
+    public int DisplayPrompt(int number=null)
     {
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(0, _prompt.Count);
-        string _promptStr = _prompt[number];
-        _promptChosen = _promptStr;
-        Console.WriteLine(_promptStr);
+        if (NumberFormatInfo != null)
+        {
+            string _promptStr = _prompt[number];
+            _promptChosen = _promptStr;
+            Console.WriteLine(_promptStr);
+            return number;
+        }
+        else
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(0, _prompt.Count);
+            string _promptStr = _prompt[number];
+            _promptChosen = _promptStr;
+            Console.WriteLine(_promptStr);
+        }
     }
 
     // Method
     public void GetInput()
     {
+        Console.Clear;
+        
         string _userResponse = Console.ReadLine();
         response.Add(_userResponse);
     }
@@ -42,7 +56,17 @@ class Listing
     }
     public void Run()
     {
-        
+        DateTime startTime = DateTime.Now;
+        DateTime endTime = startTime.AddSeconds(_time);
+        _promptDisp = DisplayPrompt();
+        while (startTime < endTime)
+        {
+            Console.Clear;
+            DisplayPrompt(number);
+            GetInput();
+            startTime = DateTime.Now;
+        }
+        DisplayAll();
     }
 
 }
