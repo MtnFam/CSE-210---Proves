571c19eb2e72a923a106facaf4a729bfa94284d8
Sean Gillman <fruit@ThinkingRocks.(none)>
2026-06-25 17:40:16 -0600

    Minor bug fixes

--- EternalQuest/GoalSaver.cs
+++ EternalQuest/GoalSaver.cs
@@ -4,10 +4,30 @@ class GoalSaver
     private List<Goal> _goallist;
     private string _filename;
     public GoalSaver(string _filename)
-    {
-        
-        File.ReadLines(_filename).First();
+    {   
+        _ptcounter = int.Parse(File.ReadLines(_filename).First());
+        _goallist = readGoals(_filename);
     }
+    public List<Goals> readGoals(string filename)
+    {
+        int lineNumber = 0;
+
+            foreach (string line in File.ReadLines(library))
+            {
+                lineNumber++;
+                string[] parts = line.Split('|', 2);
+                
+                if (parts.Length > 0 &&
+                    parts[0].Equals(sref, StringComparison.OrdinalIgnoreCase))
+                {
+                    return GetScript(library,lineNumber);
+                }
+            }
+           throw new ArgumentException($"Scripture reference '{sref}' was not found.");
+        }
+
+            List<string> parts = line.Split('|', 2).ToList();
+        }
 }
 /*Random num = new Random();
         var lines = File.ReadAllLines(libname);
